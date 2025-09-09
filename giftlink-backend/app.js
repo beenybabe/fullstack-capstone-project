59212938899c16315a52982c48c95dda5ef01656
beenybabe <beenykay@gmail.com>
2025-09-09 14:45:49 -0400

    adding page code

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -36,10 +36,11 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes);
+app.use('/api/auth', authRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/search', searchRoutes);
 
 
 // Global Error Handler
